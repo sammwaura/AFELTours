c6530f9eaae4980466aff3755316b9a350561a2f
sammwaura <meshsami27@gmail.com>
2023-08-14 14:33:42 +0300

    modify files and create new image files

--- afeltours/src/Pages/Home.jsx
+++ afeltours/src/Pages/Home.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import '../styles/home.css';
+import { Container, Row, Col } from 'reactstrap';
+import heroImg from '../assets/images/hero.jpg';
+import heroImg2 from '../assets/images/hero2.avif';
+// import heroVid from '../assets/images/heroVid.mp4';
+import worldImg from '../assets/images/world.jpg';
+import Subtitle from '../shared/Subtitle';
+
 
 const Home = () => {
-  return <div>Home</div>;
+  return <>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6'>
+            <div className="hero__content">
+              <div className="hero__subtitle d-flex align-items-center ">
+                <Subtitle subtitle={'Know Before You Go'} />
+                <img src={worldImg} alt="" />
+              </div>
+              <h1>Travelling opens the door to creating
+                <span className='highlight'> memories</span> </h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt itaque pariatur quia excepturi numquam est rem architecto quaerat, odio tenetur dolorum optio possimus officiis veniam illo officia obcaecati debitis laboriosam.</p>
+
+
+            </div>
+
+          </Col>
+        </Row>
+      </Container>
+    </section>
+    { /* hero start */}
+
+  </>
 };
 
 export default Home;
--- afeltours/src/components/Header/Header.jsx
+++ afeltours/src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Container, Row, Button } from 'reactstrap';
-import {Navlink, Link} from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 import logo from '../../assets/images/logo.png';
+import './header.css';
  
-const nav_links = [
+const nav__links = [
   {
     path:'/home',
     display:'Home'
   },
   {
-    path: '#',
+    path: '/about',
     display:'About'
   },
   {
@@ -28,6 +29,38 @@ const Header = () => {
           <div className='logo'>
             <img src={logo} alt="" />
           </div>
+          { /* logo end */ }
+
+          { /* menu start */ }
+            <div className="navigation">
+              <ul className="menu d-flex align-items-center gap-5">
+                {
+                  nav__links.map((item,index)=>(
+                    <li className='nav__item' key={index}>
+                      <NavLink
+                       to={item.path} 
+                       className={navClass=> 
+                       navClass.isActive ? 'active__link' : ''
+                       }
+                       >
+                        {item.display}
+                        </NavLink>
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+           { /* menu end */ }
+           <div className='nav__right d-flex align-items-center gap-4 '>
+            <div className='nav__btns d-flex align-items-center gap-4 '>
+              <Button className='btn .primary__btn'><Link to='/login'>Login</Link></Button>
+              <Button className='btn .secondary__btn'><Link to='/register'>Register</Link></Button>
+            </div>
+            
+            <span className='mobile__menu'>
+            <i class="ri-menu-line"></i>
+            </span>
+           </div>
         </div>
       </Row>
       </Container>
