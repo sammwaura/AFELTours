988f2dddd71c603c3861bb7ee6d2c9d65401ee01
sammwaura <meshsami27@gmail.com>
2023-08-14 17:20:19 +0300

    modify files and add video

--- afeltours/src/Pages/Home.jsx
+++ afeltours/src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import '../styles/home.css';
 import { Container, Row, Col } from 'reactstrap';
 import heroImg from '../assets/images/hero.jpg';
 import heroImg2 from '../assets/images/hero2.avif';
-// import heroVid from '../assets/images/heroVid.mp4';
+import heroVid from '../assets/images/heroVid.mp4';
 import worldImg from '../assets/images/world.jpg';
 import Subtitle from '../shared/Subtitle';
 
@@ -21,17 +21,30 @@ const Home = () => {
               </div>
               <h1>Travelling opens the door to creating
                 <span className='highlight'> memories</span> </h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt itaque pariatur quia excepturi numquam est rem architecto quaerat, odio tenetur dolorum optio possimus officiis veniam illo officia obcaecati debitis laboriosam.</p>
-
-
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt itaque pariatur quia excepturi numquam est rem architecto quaerat, 
+                  odio tenetur dolorum optio possimus officiis veniam illo officia obcaecati debitis laboriosam.</p>
             </div>
+          </Col>
 
+          <Col lg='2'>
+            <div className="hero__img-box">
+              <img src={heroImg} alt="" />
+            </div>
+          </Col>
+          <Col lg='2'>
+            <div className="hero__img-box">
+              <video src={heroVid} alt="" controls/>
+            </div>
+          </Col>
+          <Col lg='2'>
+            <div className="hero__img-box">
+              <img src={heroImg2} alt="" />
+            </div>
           </Col>
         </Row>
       </Container>
     </section>
     { /* hero start */}
-
   </>
 };
 
