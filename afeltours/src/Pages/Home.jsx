d124b754cda916c3ca0c858529adc1450bdfa2a6
sammwaura <meshsami27@gmail.com>
2023-08-15 16:07:30 +0300

    create new files & modify

--- afeltours/src/Pages/Home.jsx
+++ afeltours/src/Pages/Home.jsx
@@ -7,6 +7,8 @@ import heroVid from '../assets/images/heroVid.mp4';
 import worldImg from '../assets/images/world.jpg';
 import Subtitle from '../shared/Subtitle';
 
+import SearchBar from '../shared/SearchBar';
+
 
 const Home = () => {
   return <>
@@ -19,8 +21,8 @@ const Home = () => {
                 <Subtitle subtitle={'Know Before You Go'} />
                 <img src={worldImg} alt="" />
               </div>
-              <h1>Travelling opens the door to creating
-                <span className='highlight'> memories</span> </h1>
+              <h1>AFELTours 
+                <span className='highlight'> converging nature & leisure</span> </h1>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt itaque pariatur quia excepturi numquam est rem architecto quaerat, 
                   odio tenetur dolorum optio possimus officiis veniam illo officia obcaecati debitis laboriosam.</p>
             </div>
@@ -32,15 +34,17 @@ const Home = () => {
             </div>
           </Col>
           <Col lg='2'>
-            <div className="hero__img-box">
+            <div className="hero__img-box mt-4">
               <video src={heroVid} alt="" controls/>
             </div>
           </Col>
           <Col lg='2'>
-            <div className="hero__img-box">
+            <div className="hero__img-box mt-5">
               <img src={heroImg2} alt="" />
             </div>
           </Col>
+
+          <SearchBar />
         </Row>
       </Container>
     </section>
