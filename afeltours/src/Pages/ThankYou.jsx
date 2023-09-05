e3f6bd602d25cc9ee7dab16ef22aa60b1d519416
sammwaura <meshsami27@gmail.com>
2023-09-05 12:04:14 +0300

    create login and register pages

--- afeltours/src/Pages/ThankYou.jsx
+++ afeltours/src/Pages/ThankYou.jsx
@@ -8,13 +8,13 @@ const ThankYou = () => {
   return <section>
     <Container>
         <Row>
-            <Col lg='12'>
+            <Col lg='12' className='pt-5 text-center'>
                 <div className="thank__you">
-                    <span> <i class='ri-checkbox-circle-line'></i> </span>
+                    <span> <i class='ri-checkbox-circle-line'></i></span>
                     <h1 className="mb-3 fw-semibold">Thank You</h1>
                     <h3 className='mb-4'>your is tour is successfully booked!</h3>
 
-                    <Button className='btn primary__btn w-25'><Link>Back to Home</Link></Button>
+                    <Button className='btn primary__btn w-25'><Link to='/home'>Back to Home</Link></Button>
                 </div>
 
             </Col>
--- afeltours/src/Pages/TourDetails.jsx
+++ afeltours/src/Pages/TourDetails.jsx
@@ -6,6 +6,7 @@ import tourData from '../assets/data/tours';
 import calculateAvgRating from '../utilis/avgRating';
 import  avatar from "../assets/images/ava1.jpg";
 import Booking from '../components/Booking/Booking';
+import Newsletter from '../shared/Newsletter';
 
 
 const TourDetails = ()  => {
@@ -132,6 +133,7 @@ const TourDetails = ()  => {
         </Row>
       </Container>
     </section>
+    <Newsletter />
     </>
   );
 };
