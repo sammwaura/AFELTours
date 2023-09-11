a9b2cbcddbd1cb071acdaff0a0d9e3ae9def0b0d
sammwaura <meshsami27@gmail.com>
2023-09-11 12:47:42 +0300

    mongoDB connected

--- Backend/index.js
+++ Backend/index.js
@@ -9,9 +9,12 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 //database connection
+mongoose.connect("mongodb+srv://meshsami27:wamichwe01@cluster0.v3btrzr.mongodb.net/?retryWrites=true&w=majority");
 mongoose.set("strictQuery", false);
+mongoose.set('bufferCommands', false);
 
-const connect = async () =>{
+
+const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI,
         {
@@ -21,8 +24,13 @@ const connect = async () =>{
         console.log("MongoDB database connected");
 
     } catch (error) {
-        console.log("MongoDB database not connected. Try again");     
+        console.log("MongoDB database not connected. Try again"); 
     }
+
+    mongoose.connection.on('error', err => {
+        console.log(err);
+      });
+    
 };
 
 //middleware
